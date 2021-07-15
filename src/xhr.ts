67c7184b3aa40427289793559b21c88ad3eebe6c
hujx <hujx@zylliondata.com>
2021-07-15 22:36:10 +0800

    feat: 处理相应数据，promise封装

--- src/xhr.ts
+++ src/xhr.ts
@@ -1,20 +1,41 @@
-import { AxiosRequestConfig } from './types/index'
+import { AxiosPromise, AxiosRequestConfig, AxiosResponse } from './types/index'
  
-export default function xhr(config: AxiosRequestConfig) {
-  const { data = null, url, method = 'get', headers = {} } = config
+export default function xhr(config: AxiosRequestConfig): AxiosPromise {
+  return new Promise((resolve, reject) => {
 
-  const request = new XMLHttpRequest()
+    const { data = null, url, method = 'get', headers = {}, responseType } = config
 
-  request.open(method.toLocaleUpperCase(), url, true)
+    const request = new XMLHttpRequest()
+  
+    request.open(method.toLocaleUpperCase(), url, true)
 
-  Object.keys(headers).forEach(name => {
-    if (data === null && name.toLocaleLowerCase() === 'content-type') {
-      // 为什么 data 没有值的时候，设置请求头 ’content-type‘ 是没有意义的呢？
-      delete headers[name]
-    } else {
-      request.setRequestHeader(name, headers[name])
+    request.onreadystatechange = function handleLoad() {
+      if (request.readyState !== 4) {
+        return
+      }
+
+      const responseHeaders = request.getAllResponseHeaders()
+      const responseData = responseType && responseType !== 'text' ? request.response : request.responseText
+      const response: AxiosResponse = {
+        data: responseData,
+        status: request.status,
+        statusText: request.statusText,
+        headers: responseHeaders,
+        config,
+        request
+      }
+      resolve(response)
     }
+  
+    Object.keys(headers).forEach(name => {
+      if (data === null && name.toLocaleLowerCase() === 'content-type') {
+        // 为什么 data 没有值的时候，设置请求头 ’content-type‘ 是没有意义的呢？
+        delete headers[name]
+      } else {
+        request.setRequestHeader(name, headers[name])
+      }
+    })
+  
+    request.send(data)
   })
-
-  request.send(data)
 }
